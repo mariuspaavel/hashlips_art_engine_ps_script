825022b46c8f9f88d63acc0adb75356ee1b16e1a
Marius Paavel <marius.paavel@datel.ee>
2022-04-26 23:11:40 +0300

    improvements

--- generate.js
+++ generate.js
@@ -9,21 +9,23 @@ function main() {
 
   if (!continueConfirmation) return;
 
-  var supply = prompt("How many images do you want to generate?", "10");
+  var supply = prompt("How many images do you want to generate per group?", "10");
 
   var name = prompt("What is the name of your collection?", "");
 
   var description = prompt("What is the description for your collection?", "");
 
+  var superGroups = app.activeDocument.layerSets;
+
   alert(
-    supply +
+    parseInt(supply) * superGroups.length +
       " images will be generated, so sit back relax and enjoy the art being generated."
   );
 
-  var groups = app.activeDocument.layerSets;
-
-  resetLayers(groups);
-
+	for(var i = 0; i < superGroups.length; i++){
+		resetLayers(superGroups[i].layerSets);
+	}
+  
   function getRWeights(_str) {
     var weight = Number(_str.split("#").pop());
     if(isNaN(weight)){
@@ -35,46 +37,69 @@ function main() {
   function cleanName(_str) {
     return _str.split("#").shift();
   }
-
-  for (var h = 1; h < parseInt(supply) + 1; h++) {
-    var obj = {};
-    obj.name = name + " #" + h;
-    obj.description = description;
-    obj.image = "To be replaced";
-    obj.edition = h;
-    obj.attributes = [];
-    for (var i = 0; i < groups.length; i++) {
-      var totalWeight = 0;
-      var layerMap = [];
-
-      for(var j = 0; j < groups[i].layers.length; j++){
-        totalWeight += getRWeights(groups[i].layers[j].name);
-        layerMap.push({
-          index: j,
-          name: cleanName(groups[i].layers[j].name),
-          weight: getRWeights(groups[i].layers[j].name)
-        });
-      }
-
-      var ran = Math.floor(Math.random() * totalWeight);
-
-      (function() {
-        for(var j = 0; j < groups[i].layers.length; j++){
-          ran -= layerMap[j].weight;
-          if(ran < 0) {
-            groups[i].layers[j].visible = true;
-            obj.attributes.push({
-              trait_type: groups[i].name, 
-              value: layerMap[j].name
-            })
-            return;
-          }
-        }
-      })();
+  
+  function shuffledIndexes(length) {
+	var array = [];
+	for(var i = 1; i <= length; i++){
+		array.push(i);
+	} 
+	
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
-    saveImage(obj.edition);
-    saveMetadata(obj);
-    resetLayers(groups);
+	return array;
+}
+	
+  var totalPerGroup = parseInt(supply);
+  var shuffledIndexes = shuffledIndexes(totalPerGroup * superGroups.length);
+
+  for (var groupIndex = 0; groupIndex < superGroups.length; groupIndex++){
+	  for (var h = groupIndex * totalPerGroup; h < groupIndex * totalPerGroup + totalPerGroup; h++) {
+		var obj = {};
+		obj.name = name + " #" + (h + 1);
+		obj.description = description;
+		obj.image = "To be replaced";
+		obj.edition = shuffledIndexes[h];
+		obj.attributes = [];
+		
+		var groups = superGroups[groupIndex].layerSets;
+		
+		for (var i = 0; i < groups.length; i++) {
+		  var totalWeight = 0;
+		  var layerMap = [];
+
+		  for(var j = 0; j < groups[i].layers.length; j++){
+			totalWeight += getRWeights(groups[i].layers[j].name);
+			layerMap.push({
+			  index: j,
+			  name: cleanName(groups[i].layers[j].name),
+			  weight: getRWeights(groups[i].layers[j].name)
+			});
+		  }
+
+		  var ran = Math.floor(Math.random() * totalWeight);
+
+		  (function() {
+			for(var j = 0; j < groups[i].layers.length; j++){
+			  ran -= layerMap[j].weight;
+			  if(ran < 0) {
+				groups[i].layers[j].visible = true;
+				obj.attributes.push({
+				  trait_type: groups[i].name, 
+				  value: layerMap[j].name
+				})
+				return;
+			  }
+			}
+		  })();
+		}
+		saveImage(obj.edition);
+		saveMetadata(obj);
+		resetLayers(groups);
+	  }
   }
   alert("Generation process is complete.");
 }
